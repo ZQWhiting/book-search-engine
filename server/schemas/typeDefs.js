86d720ff2e088f84a29689c8536c395c97bc8322
ZQWhiting <zach.whiting@icloud.com>
2020-09-23 11:18:48 -0600

    created typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql``;
+const typeDefs = gql`
+	type Auth {
+		token: ID!
+		user: User
+	}
+
+	type Book {
+		bookId: ID
+		authors: [String]
+		description: String
+		title: String
+		image: String
+		link: String
+	}
+
+	type User {
+		_id: ID
+		username: String
+		email: String
+		bookCount: Int
+		savedBooks: [Book]
+	}
+
+	type Query {
+		me: User
+	}
+
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveBook(book: [Book]): User
+		removeBook(bookId: ID!): User
+	}
+`;
 
 module.exports = typeDefs;
